2669ae6a0a1c487c24a03202584ae65a0f030abe
Koushik Satyavarapu <koushik.satyavarapu@gmail.com>
2026-06-02 20:23:55 +0530

    fix backed links

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,7 @@ try:
     nltk.data.find('sentiment/vader_lexicon.zip')
 except LookupError:
     nltk.download('vader_lexicon', quiet=True)
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.news import router as news_router
@@ -11,9 +12,14 @@ from app.api.user_actions import router as user_router
 
 app = FastAPI(title="NewsPulse - AI News Trend Analyzer")
 
+# UPDATE THIS LIST: Add your specific Vercel URL here!
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173", 
+        "http://127.0.0.1:5173",
+        "https://news-pulse-1-h5x3df2jo-koushik-satyavarapus-projects.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
